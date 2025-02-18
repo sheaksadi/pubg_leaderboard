c9e684dc3bb4d8ac069ff5ae04eb78e98e5f2877
sheaksadi <75621103+sheaksadi@users.noreply.github.com>
2025-02-19 01:39:37 +0600

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+
+const store = useTestStore()
+console.log(await store.getHello())
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</template>
+
+<style scoped>
+
+</style>
